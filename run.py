94cf18b00906d4d3c2cecc0e5bc7fcad6dcd1381
Kyle Mathewson <kylemath@gmail.com>
2020-04-22 16:56:33 -0700

    alberta health regions over time

--- run.py
+++ run.py
@@ -1,23 +1,27 @@
-import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly 
 import plotly.figure_factory as ff 
 from datetime import timedelta
+import matplotlib.pyplot as plt 
+
 
 df = pd.read_csv('../downloads/covid19dataexport.csv',
 	names=['number', 'Start', 'zone', 'gender', 'age', 'Task', 'confirmed'], skiprows=1, parse_dates=['Start'])
 
 df['Finish'] = df['Start'] + timedelta(days=14)
 
-fig = ff.create_gantt(df.sort_values(by='Start', ascending='True'), bar_width=.3, height=6000, index_col='Task')
+# fig = ff.create_gantt(df.sort_values(by='Start', ascending='True'), bar_width=.3, height=6000, index_col='Task')
 
+# fig.update_layout(
+#     yaxis = {'autorange': 'reversed'}
+# )
 
+# fig.show()
+zones = ['Edmonton Zone', 'Calgary Zone', 'North Zone', 'Central Zone', 'South Zone']
 
-fig.update_layout(
-    yaxis = {
-        'autorange': 'reversed',
-    }
-)
+for zone in zones: 
+	df[df['zone'] == zone].groupby('Start').count().confirmed.plot()
 
-fig.show()
+plt.legend(zones)
+plt.ylabel('New daily cases')
+plt.show()
